7a4258abaf5b1310e4ab49cf8109f7125560bc69
KaiUtterback <156825843+KaiUtterback@users.noreply.github.com>
2024-02-28 10:21:15 -0600

    task one save

--- functions_assignment.py
+++ functions_assignment.py
@@ -11,5 +11,36 @@ Task 3: Add error handling to ensure that the user inputs a string for the name 
 
 # make a function that asks for the user's name and greets them
 # use if statements to make responses to user input
-# use try statement to test user input and respond with an 'error' message if they enter a wrong answer or data type.
+# use try / except statement to test user input and respond with an 'error' message if they enter a wrong answer or data type.
 
+def greeting():
+    greet = input("Hello, my name is Kai, what is your name? ")
+    print(f"Hello {greet}! Nice to meet you!")
+    while True:
+        feeling = input("How are you feeling today?\n (common responses are happy, sad, confused, tired, upset) ")
+
+        try:
+            feeling == str(feeling)
+        except:
+            print("I'm sorry, that's not an answer that I'm familiar with, can you try again?\n note, all inputs are case sensitive")
+            continue
+
+        if feeling == 'happy':
+            print("I'm glad you're feeling happy! I hope that your day continues to go well!")
+            break
+        if feeling == 'sad':
+            print("I'm sorry that you're feeling sad, I hope that your day gets better!")
+            break
+        if feeling == 'confused':
+            print("Sometimes I get confused too,\n usually that means I need to take a break and have some coffee")
+            break
+        if feeling == 'tired':
+            print("When I'm tired I take a nap! That always helps me!")
+            break
+        if feeling == 'upset': 
+            print("When I get upset or frusterated I know it's time to take a break from whatever I'm doing.\n Maybe you need to go outside and take a walk?")
+            break
+        else:
+            print("I'm not really used to that feeling, but I'm here for you if you need someone to talk to.")
+
+greeting()
