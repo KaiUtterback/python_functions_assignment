e8e14c76a7eb5da32ecee0558b31fee5e98fd85d
KaiUtterback <156825843+KaiUtterback@users.noreply.github.com>
2024-02-28 09:52:40 -0600

    beginning assignment

---
+++ functions_assignment.py
@@ -0,0 +1,15 @@
+# 1 The Welcoming Program
+
+'''
+Create a simple program that greets users and responds based on their input
+
+Task 1: Code a program  that asks for the user's name and prints a personalized greeting.
+Task 2: Modify the program to ask the user how they are feeling today and respond with a comforting message if they're feeling down, 
+or a cheerful one if they're feeling happy.
+Task 3: Add error handling to ensure that the user inputs a string for the name and not anumber or special character.
+'''
+
+# make a function that asks for the user's name and greets them
+# use if statements to make responses to user input
+# use try statement to test user input and respond with an 'error' message if they enter a wrong answer or data type.
+
